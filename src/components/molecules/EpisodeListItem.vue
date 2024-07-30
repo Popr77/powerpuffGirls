b599e83053e36f24f8b4ff843f8402d70c82542f
popr77 <joaopaulo.vasconcelos@hotmail.com>
2024-07-30 23:47:12 +0100

    feature: Added enumeration to episodes, scroll snap and container to layout to tvShowListView

--- src/components/molecules/EpisodeListItem.vue
+++ src/components/molecules/EpisodeListItem.vue
@@ -2,18 +2,20 @@
 import DOMPurify from 'isomorphic-dompurify';
 import type { TEpisode } from '@/types';
 
-defineProps<{ data: TEpisode }>();
+const props = defineProps<{ data: TEpisode; index: number }>();
 
 const getFormattedDate = (timestamp: string): string => {
   const date = new Date(timestamp);
 
   return `${date.getUTCDate()}/${date.getUTCMonth() + 1}/${date.getUTCFullYear()}`;
 };
+
+const nameWithEnumeration = `${props.index + 1}. ${props.data.name}`;
 </script>
 <template>
   <a
     :href="data.url"
-    class="flex gap-10 rounded border-2 p-4 duration-300 ease-in-out hover:cursor-pointer hover:border-slate-300 hover:bg-slate-50"
+    class="flex snap-start flex-wrap gap-5 rounded border-2 p-4 duration-300 ease-in-out hover:cursor-pointer hover:border-slate-300 hover:bg-slate-50 md:gap-7"
   >
     <div class="min-h-36 w-64">
       <img
@@ -24,8 +26,8 @@ const getFormattedDate = (timestamp: string): string => {
       />
       <div v-else class="h-full w-full rounded bg-slate-200 object-cover"></div>
     </div>
-    <div class="flex flex-1 flex-col">
-      <span class="text-lg font-bold">{{ data.name }}</span>
+    <div class="flex flex-1 flex-col gap-1">
+      <span class="text-lg font-bold">{{ nameWithEnumeration }}</span>
       <div class="text-sm" v-html="DOMPurify.sanitize(data.summary)"></div>
 
       <div class="flex h-full flex-col justify-end">
--- src/views/TvShowListView.vue
+++ src/views/TvShowListView.vue
@@ -13,27 +13,32 @@ tvShowStore.syncTvShowData();
 </script>
 
 <template>
-  <div class="p-10">
+  <div class="container">
     <div v-if="tvShowStore.tvShowData" class="flex gap-10 max-lg:flex-col">
       <a
         :href="tvShowStore.tvShowData.url"
-        class="relative h-fit max-w-64 max-lg:mx-auto lg:max-w-60"
+        class="relative h-fit min-w-64 max-w-96 max-lg:mx-auto lg:max-w-60"
       >
         <div
-          class="lef-0 absolute top-0 z-1 h-full w-full opacity-20 duration-300 ease-in-out hover:rounded hover:bg-black"
+          class="lef-0 absolute top-0 z-1 h-full w-full opacity-30 duration-300 ease-in-out hover:rounded hover:bg-black"
         ></div>
         <img class="w-full" :src="tvShowStore.tvShowData.image.original" alt="tvShowImage" />
       </a>
       <div class="flex flex-col gap-4">
         <h2 class="text-3xl font-bold">{{ tvShowStore.tvShowData.name }}</h2>
         <div v-html="DOMPurify.sanitize(tvShowStore.tvShowData.summary)"></div>
-        <div class="flex max-h-[75vh] flex-col gap-2 overflow-y-auto pr-2">
-          <template v-for="(episode, index) in tvShowStore.tvShowData.episodes" :key="index">
-            <EpisodeListItem :data="episode" />
-          </template>
+        <div>
+          <h2 class="text-xl font-bold">Episodes:</h2>
+          <div
+            class="mt-2 flex max-h-[90vh] snap-y snap-mandatory flex-col gap-2 overflow-y-auto pr-2 lg:max-h-[75vh]"
+          >
+            <template v-for="(episode, index) in tvShowStore.tvShowData.episodes" :key="index">
+              <EpisodeListItem :data="episode" :index="index" />
+            </template>
+          </div>
         </div>
       </div>
     </div>
-    <div v-else>It was not possible to obtain the tv show data</div>
+    <div v-else>It was not possible to obtain the tv show information</div>
   </div>
 </template>
