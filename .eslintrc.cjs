fe2d2cdb6018fde936385022dd913b3a97397c5c
popr77 <joaopaulo.vasconcelos@hotmail.com>
2024-07-30 22:09:45 +0100

    feature: Update base configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['@typescript-eslint'],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
-
-  plugins: []
-}
+  theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      xs: '480px',
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1400px',
+    },
+    extend: {
+      zIndex: {
+        1: '1',
+      },
+    },
+  },
+  plugins: [],
+};
